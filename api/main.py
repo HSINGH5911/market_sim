116da29ac1b2b089f33b58e474c8f1bff90607f5
HSINGH5911 <harshcv16@gmail.com>
2026-06-30 23:38:29 -0500

    got basic frontend actually working. will add actual stock pricings

--- api/main.py
+++ api/main.py
@@ -12,7 +12,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"],
+    allow_origins=[
+        "http://127.0.0.1:5173", 
+        "http://localhost:5173", 
+        "http://127.0.0.1:5174", 
+        "http://localhost:5174",
+        "http://127.0.0.1:5175", 
+        "http://localhost:5175"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,7 +38,7 @@ def get_stocks():
 
 @app.get("/orderbook/{ticker}")
 def get_orderbook(ticker: str):
-    book = exchange.order_books[ticker]
+    book = exchange.order_books[ticker.upper()]
 
     return {
         "bids" : [
